43369371b9a955ea9a5ff17b9bf3441f7675a2ec
Master Zan <muhammadfauzanpratama420@gmail.com>
2025-07-28 22:14:57 +0700

    not finish

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const toggleBtn = document.getElementById("toggleBtn");
+const moreText = document.getElementById("more-text");
+const dots = document.getElementById("dots");
+
+toggleBtn.addEventListener("click", function () {
+  if (moreText.classList.contains("hidden")) {
+    moreText.classList.remove("hidden");
+    dots.style.display = "none";
+    toggleBtn.textContent = "Sembunyikan";
+  } else {
+    moreText.classList.add("hidden");
+    dots.style.display = "inline";
+    toggleBtn.textContent = "Lihat Selengkapnya";
+  }
+});
